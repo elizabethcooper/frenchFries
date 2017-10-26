d142b17db9ce4043208bb40f5301a691b4e57b40
Melvin Mathew <melvin15may@gmail.com>
2017-10-26 10:00:05 -0700

    added django views and workflow

--- fries/urls.py
+++ fries/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 urlpatterns = [
     #url(r'^datetime/', views.current_date_time, name='current_date_time'),
-    url(r'^login/', views.login, name='login'),
-    url(r'^register/', views.register, name='register'),
     url(r'^welcome/', views.welcome, name='welcome'),
     url(r'^register/new', views.register_new_user, name='register_new_user'),
     url(r'^login/user', views.login_user, name='login_user'),
+    url(r'^login/', views.login, name='login'),
+    url(r'^register/', views.register, name='register')
 ]
--- fries/views.py
+++ fries/views.py
@@ -6,28 +6,33 @@ from .models import User
 
 
 def login(request):
-	return render(request, 'index.html', {'message': ''})
+    return render(request, 'index.html', {'message': ''})
 
 
 def register(request):
-	return render(request, 'register.html')
+    return render(request, 'register.html')
 
 
 def welcome(request):
-	return render(request, 'welcome.html')
-
-
-def register_new_user(request):
-	username = request.POST.get("username")
-	name = request.POST.get("name")
-	email = request.POST.get("email")
-	user = User.objects.create(name=name, user_name=username, email=email)
-	return HttpResponse(json.dumps(user.values()), content_type="application/json")
-
-def login_user(request, username=None):
-	username = username if username is None  else request.GET.get('username')
+	username = request.GET.get('username', None)
 	if User.objects.filter(user_name=username).exists():
-		return redirect('/fries/welcome')
+		return render(request, 'welcome.html', {'username': username})
 	else:
 		return render(request, 'index.html', {'message': 'Username not valid'})
 
+
+def register_new_user(request):
+    username = request.POST.get("username")
+    name = request.POST.get("name")
+    email = request.POST.get("email")
+    user = User.objects.create(name=name, user_name=username, email=email)
+    return redirect('/fries/welcome?username=' + username)
+
+
+def login_user(request, username=None):
+    username = username if username is not None else request.POST.get(
+        'username')
+    if User.objects.filter(user_name=username).exists():
+        return redirect('/fries/welcome?username=' + username)
+    else:
+        return render(request, 'index.html', {'message': 'Username not valid'})
