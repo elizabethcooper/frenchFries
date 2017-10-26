eec0879ca5032ac68c71cc24a9d67bcdc1ca57d0
Melvin Mathew <melvin15may@gmail.com>
2017-10-26 07:31:28 -0700

    added index.html

--- fries/urls.py
+++ fries/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^datetime/', views.current_date_time, name='current_date_time'),
+    url(r'^page/', views.current_date_time_page, name='current_date_time_page'),
 ]
